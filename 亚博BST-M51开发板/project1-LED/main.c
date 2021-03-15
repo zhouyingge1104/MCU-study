84e3a152639a3036b6df010f9a5cbba25fef1d35
Yingge Zhou <773753415@qq.com>
2021-03-15 18:59:10 +0800

    Init commit.
    Init commit.

---
+++ 亚博BST-M51开发板/project1-LED/main.c
@@ -0,0 +1,26 @@
+#include<reg52.h>
+//#define led P0
+sbit led1 = P1^0;
+sbit led2 = P1^1;
+sbit led3 = P1^2;
+sbit led4 = P1^3;
+sbit led5 = P1^4;
+sbit led6 = P1^5;
+sbit led7 = P1^6;
+sbit led8 = P1^7;
+
+void main()
+{
+	while(1)
+	{
+		led1 = 0; //light the first LED
+		led2 = 0; 
+		led3 = 0;
+		led4 = 0;
+		led5 = 0;
+		led6 = 0;
+		led7 = 0;
+		led8 = 0;
+		
+	}
+}
